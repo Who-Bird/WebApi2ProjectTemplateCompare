cbb87806459e7496e5ee5fdabf0cab6fda1e644e
Will 保哥 <doggy.huang@gmail.com>
2016-09-22 17:03:21 +0800

    加入 ASP.NET 4.5.2 範本：MVC + Web API 核心參考

--- WebApi2ProjectTemplateCompare/Global.asax
+++ WebApi2ProjectTemplateCompare/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApi2ProjectTemplateCompare.WebApiApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApi2ProjectTemplateCompare.MvcApplication" Language="C#" %>
---
+++ WebApi2ProjectTemplateCompare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApi2ProjectTemplateCompare.Startup))]
+namespace WebApi2ProjectTemplateCompare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
